990554a4ca3b2dab9e770c3331735d1bef5352f1
vamshik-1 <121558872+vamshik-1@users.noreply.github.com>
2024-03-22 21:22:17 +0530

    Add files via upload

---
+++ majorproject.py
@@ -0,0 +1,513 @@
+from django.contrib import admin
+from django.apps import AppConfig
+
+
+class ClientSiteConfig(AppConfig):
+    name = 'Remote_User'
+from django import forms
+
+from Remote_User.models import ClientRegister_Model
+
+
+class ClientRegister_Form(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = ClientRegister_Model
+        fields = ("username","email","password","phoneno","country","state","city")
+
+from django.db import models
+
+# Create your models here.
+from django.db.models import CASCADE
+
+
+class ClientRegister_Model(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+    password = models.CharField(max_length=10)
+    phoneno = models.CharField(max_length=10)
+    country = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    gender= models.CharField(max_length=30)
+    address= models.CharField(max_length=30)
+
+
+class delay_prediction_type(models.Model):
+
+    ddatetime= models.CharField(max_length=300)
+    idn= models.CharField(max_length=300)
+    carrier = models.CharField(max_length=300)
+    adate1= models.CharField(max_length=300)
+    connection= models.CharField(max_length=300)
+    arrival= models.CharField(max_length=300)
+    delay_in_min= models.CharField(max_length=300)
+    name= models.CharField(max_length=300)
+    Prediction= models.CharField(max_length=300)
+
+class detection_accuracy(models.Model):
+
+    names = models.CharField(max_length=300)
+    ratio = models.CharField(max_length=300)
+
+from django.test import TestCase
+
+# Create your tests here.
+
+from django.db.models import Count
+from django.db.models import Q
+from django.shortcuts import render, redirect, get_object_or_404
+
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import VotingClassifier
+# Create your views here.
+from Remote_User.models import ClientRegister_Model,delay_prediction_type,detection_ratio,detection_accuracy
+
+def login(request):
+
+
+    if request.method == "POST" and 'submit1' in request.POST:
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            enter = ClientRegister_Model.objects.get(username=username,password=password)
+            request.session["userid"] = enter.id
+
+            return redirect('ViewYourProfile')
+        except:
+            pass
+
+    return render(request,'RUser/login.html')
+
+def index(request):
+    return render(request, 'RUser/index.html')
+
+def Add_DataSet_Details(request):
+
+    return render(request, 'RUser/Add_DataSet_Details.html', {"excel_data": ''})
+
+
+def Register1(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        phoneno = request.POST.get('phoneno')
+        country = request.POST.get('country')
+        state = request.POST.get('state')
+        city = request.POST.get('city')
+        address = request.POST.get('address')
+        gender = request.POST.get('gender')
+        ClientRegister_Model.objects.create(username=username, email=email, password=password, phoneno=phoneno,
+                                            country=country, state=state, city=city,address=address,gender=gender)
+
+        obj = "Registered Successfully"
+        return render(request, 'RUser/Register1.html',{'object':obj})
+    else:
+        return render(request,'RUser/Register1.html')
+
+def ViewYourProfile(request):
+    userid = request.session['userid']
+    obj = ClientRegister_Model.objects.get(id= userid)
+    return render(request,'RUser/ViewYourProfile.html',{'object':obj})
+
+
+def Predict_Train_Delay_Type(request):
+    if request.method == "POST":
+
+        if request.method == "POST":
+            RID = request.POST.get('RID')
+            ddatetime= request.POST.get('ddatetime')
+            idn= request.POST.get('idn')
+            carrier= request.POST.get('carrier')
+            adate1= request.POST.get('adate1')
+            connection= request.POST.get('connection')
+            arrival= request.POST.get('arrival')
+            delay_in_min= request.POST.get('delay_in_min')
+            name= request.POST.get('name')
+
+
+
+        df = pd.read_csv('Train_Datasets.csv')
+
+        def apply_response(delay):
+            if (int(delay) >=int(0) and  int(delay) <= int(5)):
+                return 0  # No Delay
+            elif (int(delay)> int(5)):
+                return 1  # More Delay
+
+        df['results'] = df['delay_in_min'].apply(apply_response)
+
+        cv = CountVectorizer()
+        X = df['RID']
+        y = df['results']
+
+        print("Connection Station")
+        print(X)
+        print("Results")
+        print(y)
+
+        X = cv.fit_transform(X)
+
+        models = []
+        from sklearn.model_selection import train_test_split
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+        X_train.shape, X_test.shape, y_train.shape
+
+        print("Naive Bayes")
+
+        from sklearn.naive_bayes import MultinomialNB
+
+        NB = MultinomialNB()
+        NB.fit(X_train, y_train)
+        predict_nb = NB.predict(X_test)
+        naivebayes = accuracy_score(y_test, predict_nb) * 100
+        print("ACCURACY")
+        print(naivebayes)
+        print("CLASSIFICATION REPORT")
+        print(classification_report(y_test, predict_nb))
+        print("CONFUSION MATRIX")
+        print(confusion_matrix(y_test, predict_nb))
+        models.append(('naive_bayes', NB))
+
+        # SVM Model
+        print("SVM")
+        from sklearn import svm
+
+        lin_clf = svm.LinearSVC()
+        lin_clf.fit(X_train, y_train)
+        predict_svm = lin_clf.predict(X_test)
+        svm_acc = accuracy_score(y_test, predict_svm) * 100
+        print("ACCURACY")
+        print(svm_acc)
+        print("CLASSIFICATION REPORT")
+        print(classification_report(y_test, predict_svm))
+        print("CONFUSION MATRIX")
+        print(confusion_matrix(y_test, predict_svm))
+        models.append(('svm', lin_clf))
+
+        print("Logistic Regression")
+
+        from sklearn.linear_model import LogisticRegression
+
+        reg = LogisticRegression(random_state=0, solver='lbfgs').fit(X_train, y_train)
+        y_pred = reg.predict(X_test)
+        print("ACCURACY")
+        print(accuracy_score(y_test, y_pred) * 100)
+        print("CLASSIFICATION REPORT")
+        print(classification_report(y_test, y_pred))
+        print("CONFUSION MATRIX")
+        print(confusion_matrix(y_test, y_pred))
+        models.append(('logistic', reg))
+
+        print("SGD Classifier")
+        from sklearn.linear_model import SGDClassifier
+        sgd_clf = SGDClassifier(loss='hinge', penalty='l2', random_state=0)
+        sgd_clf.fit(X_train, y_train)
+        sgdpredict = sgd_clf.predict(X_test)
+        print("ACCURACY")
+        print(accuracy_score(y_test, sgdpredict) * 100)
+        print("CLASSIFICATION REPORT")
+        print(classification_report(y_test, sgdpredict))
+        print("CONFUSION MATRIX")
+        print(confusion_matrix(y_test, sgdpredict))
+        models.append(('SGDClassifier', sgd_clf))
+
+        classifier = VotingClassifier(models)
+        classifier.fit(X_train, y_train)
+        y_pred = classifier.predict(X_test)
+
+        RID1 = [RID]
+        vector1 = cv.transform(RID1).toarray()
+        predict_text = classifier.predict(vector1)
+
+        pred = str(predict_text).replace("[", "")
+        pred1 = pred.replace("]", "")
+
+        prediction = int(pred1)
+
+        if (prediction == 0):
+            val = 'No Delay'
+        elif (prediction == 1):
+            val = 'More Delay'
+
+        print(val)
+        print(pred1)
+
+        delay_prediction_type.objects.create(ddatetime=ddatetime,
+        idn=idn,
+        carrier=carrier,
+        adate1=adate1,
+        connection=connection,
+        arrival=arrival,
+        delay_in_min=delay_in_min,
+        name=name,
+        Prediction=val)
+
+        return render(request, 'RUser/Predict_Train_Delay_Type.html',{'objs': val})
+    return render(request, 'RUser/Predict_Train_Delay_Type.html')
+
+
+
+from django.contrib import admin
+
+# Register your models here.
+
+from django.apps import AppConfig
+
+
+class ResearchSiteConfig(AppConfig):
+    name = 'Service_Provider'
+from django.db import models
+
+# Create your models here.
+from django.test import TestCase
+
+# Create your tests here.
+
+from django.db.models import  Count, Avg
+from django.shortcuts import render, redirect
+from django.db.models import Count
+from django.db.models import Q
+import datetime
+import xlwt
+from django.http import HttpResponse
+
+
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+
+# Create your views here.
+from Remote_User.models import ClientRegister_Model,delay_prediction_type,detection_ratio,detection_accuracy
+
+
+def serviceproviderlogin(request):
+    if request.method  == "POST":
+        admin = request.POST.get('username')
+        password = request.POST.get('password')
+        if admin == "Admin" and password =="Admin":
+            detection_accuracy.objects.all().delete()
+            return redirect('View_Remote_Users')
+
+    return render(request,'SProvider/serviceproviderlogin.html')
+
+def View_Train_Delay_Detection_Ratio(request):
+    detection_ratio.objects.all().delete()
+    ratio = ""
+    kword = 'No Delay'
+    print(kword)
+    obj = delay_prediction_type.objects.all().filter(Q(Prediction=kword))
+    obj1 = delay_prediction_type.objects.all()
+    count = obj.count();
+    count1 = obj1.count();
+    ratio = (count / count1) * 100
+    if ratio != 0:
+        detection_ratio.objects.create(names=kword, ratio=ratio)
+
+    ratio12 = ""
+    kword12 = 'More Delay'
+    print(kword12)
+    obj12 = delay_prediction_type.objects.all().filter(Q(Prediction=kword12))
+    obj112 = delay_prediction_type.objects.all()
+    count12 = obj12.count();
+    count112 = obj112.count();
+    ratio12 = (count12 / count112) * 100
+    if ratio12 != 0:
+        detection_ratio.objects.create(names=kword12, ratio=ratio12)
+
+    obj = detection_ratio.objects.all()
+    return render(request, 'SProvider/View_Train_Delay_Detection_Ratio.html', {'objs': obj})
+
+def View_Remote_Users(request):
+    obj=ClientRegister_Model.objects.all()
+    return render(request,'SProvider/View_Remote_Users.html',{'objects':obj})
+
+def charts(request,chart_type):
+    chart1 = detection_ratio.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/charts.html", {'form':chart1, 'chart_type':chart_type})
+
+def charts1(request,chart_type):
+    chart1 = detection_accuracy.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/charts1.html", {'form':chart1, 'chart_type':chart_type})
+
+def View_Prediction_Of_Train_Time_Delay(request):
+    obj =delay_prediction_type.objects.all()
+    return render(request, 'SProvider/View_Prediction_Of_Train_Time_Delay.html', {'list_objects': obj})
+
+def likeschart(request,like_chart):
+    charts =detection_accuracy.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/likeschart.html", {'form':charts, 'like_chart':like_chart})
+
+
+def Download_Trained_DataSets(request):
+
+    response = HttpResponse(content_type='application/ms-excel')
+    # decide file name
+    response['Content-Disposition'] = 'attachment; filename="Predicted_Datasets.xls"'
+    # creating workbook
+    wb = xlwt.Workbook(encoding='utf-8')
+    # adding sheet
+    ws = wb.add_sheet("sheet1")
+    # Sheet header, first row
+    row_num = 0
+    font_style = xlwt.XFStyle()
+    # headers are bold
+    font_style.font.bold = True
+    # writer = csv.writer(response)
+    obj = delay_prediction_type.objects.all()
+    data = obj  # dummy method to fetch data.
+    for my_row in data:
+        row_num = row_num + 1
+        ws.write(row_num, 0, my_row.ddatetime, font_style)
+        ws.write(row_num, 1, my_row.idn, font_style)
+        ws.write(row_num, 2, my_row.carrier, font_style)
+        ws.write(row_num, 3, my_row.adate1, font_style)
+        ws.write(row_num, 4, my_row.connection, font_style)
+        ws.write(row_num, 5, my_row.arrival, font_style)
+        ws.write(row_num, 6, my_row.delay_in_min, font_style)
+        ws.write(row_num, 7, my_row.name, font_style)
+        ws.write(row_num, 8, my_row.Prediction, font_style)
+
+    wb.save(response)
+    return response
+
+def train_model(request):
+    detection_accuracy.objects.all().delete()
+
+    df = pd.read_csv('Train_Datasets.csv')
+
+    def apply_response(delay):
+        if (int(delay) >= int(0) and int(delay) <= int(5)):
+            return 0  # No Delay
+        elif (int(delay) > int(5)):
+            return 1 # More Delay
+
+    df['results'] = df['delay_in_min'].apply(apply_response)
+
+    cv = CountVectorizer()
+    X = df['connection']
+    y = df['results']
+
+    print("Review")
+    print(X)
+    print("Results")
+    print(y)
+
+    X = cv.fit_transform(X)
+
+    csv_format = 'Results.csv'
+    df.to_csv(csv_format, index=False)
+    df.to_markdown
+
+    models = []
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+    X_train.shape, X_test.shape, y_train.shape
+
+
+    print(X_test)
+
+    print("Naive Bayes")
+
+    from sklearn.naive_bayes import MultinomialNB
+    NB = MultinomialNB()
+    NB.fit(X_train, y_train)
+    predict_nb = NB.predict(X_test)
+    naivebayes = accuracy_score(y_test, predict_nb) * 100
+    print(naivebayes)
+    print(confusion_matrix(y_test, predict_nb))
+    print(classification_report(y_test, predict_nb))
+    models.append(('naive_bayes', NB))
+    detection_accuracy.objects.create(names="Naive Bayes", ratio=naivebayes)
+
+    # SVM Model
+    print("SVM")
+    from sklearn import svm
+    lin_clf = svm.LinearSVC()
+    lin_clf.fit(X_train, y_train)
+    predict_svm = lin_clf.predict(X_test)
+    svm_acc = accuracy_score(y_test, predict_svm) * 100
+    print(svm_acc)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, predict_svm))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, predict_svm))
+    models.append(('svm', lin_clf))
+    detection_accuracy.objects.create(names="SVM", ratio=svm_acc)
+
+    print("Logistic Regression")
+
+    from sklearn.linear_model import LogisticRegression
+    reg = LogisticRegression(random_state=0, solver='lbfgs').fit(X_train, y_train)
+    y_pred = reg.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, y_pred) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, y_pred))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, y_pred))
+    models.append(('logistic', reg))
+    detection_accuracy.objects.create(names="Logistic Regression", ratio=accuracy_score(y_test, y_pred) * 100)
+
+    print("SGD Classifier")
+    from sklearn.linear_model import SGDClassifier
+    sgd_clf = SGDClassifier(loss='hinge', penalty='l2', random_state=0)
+    sgd_clf.fit(X_train, y_train)
+    sgdpredict = sgd_clf.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, sgdpredict) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, sgdpredict))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, sgdpredict))
+    models.append(('SGDClassifier', sgd_clf))
+    detection_accuracy.objects.create(names="SGD Classifier", ratio=accuracy_score(y_test, sgdpredict) * 100)
+
+
+
+    obj = detection_accuracy.objects.all()
+    return render(request,'SProvider/train_model.html', {'objs': obj})
+
+
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'train_time_delay_prediction.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
+
+
+class detection_ratio(models.Model):
+
+    names = models.CharField(max_length=300)
+    ratio = models.CharField(max_length=300)
+
+
+
